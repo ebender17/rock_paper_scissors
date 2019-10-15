143d16e3d402abc6b4421ddf0b531166ae6e7003
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-15 11:11:12 -0400

    RockPaperScissorsFrame constructor

--- src/RockPaperScissorsFrame.java
+++ src/RockPaperScissorsFrame.java
@@ -2,7 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class RockPaperScissorsFrame extends JFrame {
+    JLabel mainLabel = new JLabel("Rock Paper Scissors");
+    JPanel mainPanel = new JPanel();
+
     public RockPaperScissorsFrame(String title) {
         super(title);
+
+        add(mainPanel);
+        setSize(700,500);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
