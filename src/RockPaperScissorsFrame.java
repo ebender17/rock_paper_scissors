09ebd87b0aed6ed67cfcb705696767f103b86e03
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-16 10:10:01 -0400

    GUI components

--- src/RockPaperScissorsFrame.java
+++ src/RockPaperScissorsFrame.java
@@ -1,9 +1,38 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RockPaperScissorsFrame extends JFrame {
     JLabel mainLabel = new JLabel("Rock Paper Scissors");
+    JLabel playerWinsLabel = new JLabel("Player Wins: " + playerWins);
+    JLabel computerWinsLabel = new JLabel("Computer Wins: " + computerWins);
+    JLabel tiesLabel = new JLabel("Ties: " + ties);
+
+    //Panels
     JPanel mainPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+    JPanel statsPanel = new JPanel();
+    JPanel resultsPanel = new JPanel();
+
+    //Text area and scroll pane
+    JTextArea resultsArea = new JTextArea(1, 40);
+    JScrollPane resultsScroll = new JScrollPane(resultsArea);
+
+
+    //Buttons
+    JButton rockBtn = new JButton(new ImageIcon("img/rock_hand.png"));
+    JButton paperBtn = new JButton(new ImageIcon("img/paper_hand.png"));
+    JButton scissorsBtn = new JButton(new ImageIcon("img/scissors_hand.png"));
+    JButton quitBtn = new JButton("Quit");
+
+    //Border & Border Layout
+    Border blackline = BorderFactory.createLineBorder(Color.DARK_GRAY);
+    BorderLayout mainPanelLayout = new BorderLayout();
+    GridLayout boardPanelLayout = new GridLayout(1, 3);
+    GridLayout statsPanelLayout = new GridLayout(3, 1);
+
 
     public RockPaperScissorsFrame(String title) {
         super(title);
@@ -14,4 +43,22 @@ public class RockPaperScissorsFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
+   /* private void playGame() {
+        int playerPic;
+        int computerPic;
+
+        rockBtn.addActionListener((ActionEvent ae) -> {
+            playerPic = 1; //rock
+        });
+        paperBtn.addActionListener();
+        scissorsBtn.addActionListener();
+
+        resultsArea.append(calculateResults());
+    }*/
+
+   /* private void calculateResults() {
+
+    } */
+
+
 }
